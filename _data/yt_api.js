51afe6afb7bcce0ea7bad2c0161cb03e2ca24e9a
wommy <tom@technomad.media>
2020-02-07 16:08:18 -0500

    cleaned yt_api

--- _data/yt_api.js
+++ _data/yt_api.js
@@ -2,10 +2,15 @@ const axios = require('axios');
 
 require('dotenv').config();
 
-module.exports = async function() {
-  let url = `https://www.googleapis.com/youtube/v3/playlistItems?`
-  url += `playlistId=UURXnOs1rjfLMYrtZ-0n29NA&part=snippet&maxResults=15`
-  url += `&key=${process.env.YTAPI_KEY}`
+let url = `\
+https://www.googleapis.com/youtube/v3/playlistItems\
+?playlistId=UURXnOs1rjfLMYrtZ-0n29NA\
+&key=${process.env.YTAPI_KEY}\
+&maxResults=30\
+&part=snippet\
+`
+
+module.exports = async () => {
   try {
     const response = await axios.get(url);
     return response.data;
