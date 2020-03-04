724a7b787d62ceaca454a6322c7146aa2fe302f8
wommy <tom@technomad.media>
2020-03-04 14:03:46 -0500

    11ty.__static: menu, banner imgs

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,5 @@ module.exports = (config) => {
 	config.addFilter("byeRight", s => `${s.replace(" | FreedomToons","")}` );
 	config.addFilter("nl2br", s => `${s.replace(/\n/g, "<br>")}` );
 	config.addFilter('markdownify', s => md.renderInline(s) );
+	config.addPassthroughCopy("__static");
 }
