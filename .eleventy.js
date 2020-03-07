7cf6d914dc5e2d0035b59be9fe76e44079dcc56b
wommy <tom@technomad.media>
2020-03-06 19:13:46 -0500

    added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -4,5 +4,6 @@ module.exports = (config) => {
 	config.addFilter("byeRight", s => `${s.replace(" | FreedomToons","")}` );
 	config.addFilter("nl2br", s => `${s.replace(/\n/g, "<br>")}` );
 	config.addFilter('markdownify', s => md.renderInline(s) );
-	config.addPassthroughCopy("__static");
+	config.addPassthroughCopy("__static/img");
+	config.addPassthroughCopy({"__static/favicon":"/"});
 }
